510f5c17acb10c13d75235e80993f9ab81ce402a
Jakob Balkovec <116223977+j-balkovec@users.noreply.github.com>
2023-06-11 12:48:47 +0200

    Update lab2.cpp

--- Labs/lab2.cpp
+++ Labs/lab2.cpp
@@ -1,39 +1,56 @@
-//Jakob Balkovec
-//lab2.cpp
-
 #include <iostream>
 #include <string>
-using namespace std;
+#include <limits> // Added for std::numeric_limits
+#include <utility>
 
-int main()
-{
-  
-  string word;
-  int nm1, nm2;
-  double dnum1, dnum2;
-  
-  
-  cout << endl << endl;
-  cout << endl << endl;
-  
-  cout <<"Please enter a word: ";
-  cin >> word;
-  
-  cout <<"The word you entered is: " << word << endl;
+static inline std::pair<float, float> get_float() {
+    float f_, _f;
+    std::cout << "[provide two floats]: ";
+    std::cin >> f_ >> _f;
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // Ignore remaining newline character
 
-  cout << "Please provide two whole numbers: ";
-  cin >> nm1 >> nm2;
-  
-  cout <<"Division of " << nm1 << " by " << nm2 << " is " << nm1/nm2
-       << endl;
-  cout <<"Modulus of " << nm1 << " by " << nm2 << " is " << nm1%nm2
-       << endl;
-  
-  cout << "Please provide two floating point numbers: ";
-  cin >> dnum1 >> dnum2;
-  
-  cout << "Division of" << dnum1 << " by " << dnum2 << " is " << dnum1/dnum2
-       <<endl;
-  
-  return 0;
+    return std::make_pair(f_, _f);
+}
+
+static inline std::pair<int, int> get_int() {
+    int f_, _f;
+    std::cout << "[provide two ints]: ";
+    std::cin >> f_ >> _f;
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // Ignore remaining newline character
+
+    return std::make_pair(f_, _f);
+}
+
+static inline const char* get_word() {
+    static std::string word;
+    std::cout << "[provide a string]: ";
+    std::getline(std::cin, word);
+    return word.c_str();
+}
+
+template <typename T>
+void print(const T& value) {
+    std::cout << value << std::endl;
+}
+
+template<typename T2>
+T2 div(T2 num, T2 num_) {
+    std::cout << "[division of]: " << num << " & " << num_ << "\n";
+    return num / num_;
+}
+
+int main(/*int argc, char* argv[]*/) {
+    const char* word = get_word();
+    //print(word);
+
+    std::pair<int, int>nums = get_int();
+    std::pair<float, float>nums_ = get_float();
+
+    auto i = div(std::get<0>(nums), std::get<1>(nums));
+    print(i);
+
+    auto j = div(std::get<0>(nums_), std::get<1>(nums_));
+    print(j);
+
+    return EXIT_SUCCESS;
 }
