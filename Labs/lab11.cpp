e6fbe9c87fa5e006287f619a413d77b121c9fde0
Jakob Balkovec <116223977+j-balkovec@users.noreply.github.com>
2023-06-11 13:05:33 +0200

    Update lab11.cpp

--- Labs/lab11.cpp
+++ Labs/lab11.cpp
@@ -1,66 +1,85 @@
 // {
 //Jakob Balkovec
-//lab12.cpp
+//lab11.cpp
 //Driver Code
 // }
 
-#include <iostream> 
+#include <iostream>
 #include <fstream> 
 #include <string>  
-#include <vector>
+#include <iomanip> 
 
-static std::string const FILENAME = "/home/fac/sreeder/class/cs1420/lab12input.dat";
-static int const SIZE = 50;
-static int const DECIMAL = 3;
+const int ARRAYSIZE = 41;
+
+const int COL = 9;
+const int ROW = 6;
+
+const int COL_WIDTH = 4;
+const std::string FILENAME = "/home/fac/sreeder/class/cs1420/lab11input.dat";
 
 // {
-// Reads the file
+// Reads the file and stores the integers in an array
 // }
-static inline void read_file(std::string const FILENAME, std::vector<double> arr) {
-  std::ifstream inputFile;  
-  double number;
-  int i = 0;
-
-  try {
-    inputFile.open(FILENAME);
-    if (inputFile.good()) {
+static inline int read_file(int numbers[ARRAYSIZE]) {
+  std::ifstream input_file(FILENAME);  
+  int normalCount = 0;
+  int idx = 0;
+  
+  try{ 
+    if (input_file.good()) {
       
-      while (inputFile >> number){
-        arr.at(i) = number;
-            i++;
+      int current_number = 0;
+      while (input_file >> current_number){
+	numbers[normalCount] = current_number;
+	normalCount++;;
       }
-      inputFile.close();
+      
+      input_file.close();
+      
     }else{ 
-      throw std::runtime_error("cannot open file");
-    } 
-  } catch(std::exception &e) {
-    std::cout << "\n[error]: {" << e.what() << "}\n";
+      throw std::runtime_error("[cannot open file]");
+    }
+  }catch(std::exception &e) {
+    std::cout << "[error]: " <<e.what() << std::endl;
     std::exit(EXIT_FAILURE);
   }
-  return;
+  return idx;
 }
 
 // {
-// Computes the total 
+// Computes the sums in the array
 // }
-static inline double total(std::vector<double> arr) {
-  double total = 0;
-  for (auto iter : arr){
-    total += iter;
+static inline void get_sum(int &idx, int size, int twod_arr[ROW][COL], int numbers[ARRAYSIZE])  {
+  int sum = 0;
+  
+  for(int i = 0; i < ROW-1; i++){ //down
+    for(int j = 0; j < COL && idx<size; j++){
+      if(j == COL-1){
+	twod_arr[i][j] = sum;
+      }else {
+	twod_arr[i][j] = numbers[idx++];
+	sum += twod_arr[i][j];
+      }
     }
-  return total;
-}
-
-// {
-// Prints the array
-// }
-static inline void print_arr(std::vector<double> arr) {
-  for(auto i = 0; i < arr.size(); i++){
-    std::cout.setf(std::ios::fixed,std::ios::floatfield);
-    std::cout.precision(DECIMAL);
-    std::cout << "[index: " << i << "]:  {" << arr[i] << "}\n";
+    sum = 0;
   }
-  return;
+  
+  for (int k = 0; k < COL; k++) { //across
+    for (int l = 0; l < ROW-1; l++) {
+      sum += twod_arr[l][k];
+    }
+    twod_arr[ROW-1][k] = sum;
+    sum = 0;
+  }
+    
+  for(int a = 0; a < ROW; a++){ //std::cout
+    std::cout << std::endl;
+    for(int b = 0; b < COL; b++){
+      std::cout << std::setw(COL_WIDTH) << twod_arr[a][b] << " ";
+    }
+    std::cout << std::endl;
+  }
+  std::cout << "\n[sum of the array is in the bottom right corner: {" << twod_arr[ROW-1][COL-1] << "}]\n\n";
 }
 
 // {
@@ -68,11 +87,14 @@ static inline void print_arr(std::vector<double> arr) {
 // Returns 0 upon successful execution
 // }
 int main([[maybe_unused]] int argv, [[maybe_unused]] char* argc[]) {
-  std::vector<double> arr;
-
-  read_file(FILENAME, arr);
-  print_arr(arr);
-  std::cout << "\n[total]:  " << total(arr) << "\n\n";
+  int numbers[ARRAYSIZE] = {0};
+  int twod_arr[ROW][COL] = {0};
+  int size = sizeof(numbers)/sizeof(int);
+  int idx = read_file(numbers);
+  
+  get_sum(idx, size, twod_arr, numbers);
   
   return EXIT_SUCCESS;
 }
+
+
