d23c91008a4be7afb653aa257ecfb63a2c533ce9
Jakob Balkovec <116223977+j-balkovec@users.noreply.github.com>
2023-06-11 12:52:03 +0200

    Update lab8.cpp

--- Labs/lab8.cpp
+++ Labs/lab8.cpp
@@ -1,79 +1,86 @@
-//lab8.cpp
+// {
 //Jakob Balkovec
+//lab8.cpp
+//Driver Code
+// }
 
 #include <iostream>
 #include <string>
 
-using namespace std;
-
-void welcome();
-void goodbye();
-int input();
-int summation (int nm1);
-
-const int maxNum = 25;
-const int minNum = 5;
+const int MAX = 300;
+const int MIN = 5;
 const char YES = 'Y';
 
-int main()
-{
+// {
+// Prompts the user with a welcome message
+// }
+static inline void welcome() {
+  std::cout <<  "\n\n[welcome to Lab 8]\n";
+  return;
+}
+
+// {
+// Prompts the user with goodbye
+// }
+static inline void goodbye() {
+  std::cout << "\n[goodbye]\n ";
+  return;
+}
+
+// {
+// Gets input from the user
+// }
+static inline void get_num(int &num) {
+  try {
+    std::cout << "\n[enter a number from 5 to 300]: ";
+    std::cin >> num;
+    if(num > MAX || num < MIN) {
+      throw std::runtime_error("invalid");
+    }
+  } catch(std::exception &e) {
+    std::cout << "[error]: " << e.what() << std::endl;
+    get_num(num);
+  }
+  return;
+}
+
+// {
+// Summs the numbers from 1 to ~num~
+// }
+static inline int summation (int nm1) {
+  int sum = 0;
+  for(auto i = 1; i <= nm1; i++){
+    sum += i;
+  }
+
+  std::cout << "\n[sum from 1 to "<< nm1 << " is " << sum << "]\n\n";
+  return sum;
+}
+
+// {
+// Prompts the user to enter y or n
+// }
+static inline char get_choice() {
+  char c;
+  std::cout << "[play again(y/n)]: ";
+  std::cin >> c;
+  return c; 
+}
+
+// {
+// Main function of the program
+// Returns 0 upon successful execution
+// }
+int main([[maybe_unused]] int argv, [[maybe_unused]] char* argc[]) {
   welcome ();
   char choice;
   do{
-    
     int nm1;
-    nm1 = input();
+    get_num(nm1);
     summation(nm1);
-    
-    cout << endl;
-    cout << "Do you want to play again? (y/n) ";
-    cin >> choice;
-    cout << endl;
+    choice = get_choice();
   }while (choice == YES);
   
   goodbye();
-
-  cout << endl << endl;
-  return 0;
-}
-
-void welcome()
-{
-  cout << endl << endl;
-  cout <<  "Welcome to Lab 8! ";
-  cout << endl << endl;
-
-}
-
-void goodbye()
-{
-  cout << "Goodbye, thank you for playing! ";
-  cout << endl;
-}
-
-int input()
-{
-    int nm1;
-    cout << "Please enter a number from 5 to 25: ";
-    cin >> nm1;
-    
-    while(nm1 < minNum || nm1 > maxNum){
-      cout << "Please enter a value in-between 5 and 25: ";
-      cin >> nm1;
-      cout << endl;
-    }
-    
-    return nm1;
-}
-
-int summation (int nm1)
-{
-  int sum = 0;
-  for(int i = 1; i <= nm1; i++){
-    sum += i;
-  }
-  cout << endl;
-  cout << "The sum from 1 to "<< nm1 << " is " << sum << endl;
-  
-  return sum;
+  return EXIT_SUCCESS;
 }
